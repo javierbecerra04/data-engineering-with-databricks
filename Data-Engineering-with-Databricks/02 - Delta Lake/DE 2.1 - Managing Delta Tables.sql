d5a1701980f9fde9cff637fd9afb4c668b2b080e
javierbecerra04 <javierbecerra04>
2022-07-03 21:05:27 +0000

    Add sql for insert when its not match between temp table and prod table

--- Data-Engineering-with-Databricks/02 - Delta Lake/DE 2.1 - Managing Delta Tables.sql
+++ Data-Engineering-with-Databricks/02 - Delta Lake/DE 2.1 - Managing Delta Tables.sql
@@ -250,6 +250,18 @@ WHEN NOT MATCHED AND u.type = "insert"
 
 -- COMMAND ----------
 
+MERGE INTO students b
+USING updates u
+ON b.id=u.id
+WHEN MATCHED AND u.type = "update"
+  THEN UPDATE SET *
+WHEN MATCHED AND u.type = "delete"
+  THEN DELETE
+WHEN NOT MATCHED
+  THEN INSERT *
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC 
 -- MAGIC 
